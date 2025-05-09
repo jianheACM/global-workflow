b1511b02aa22b1cacf04ed1365dad513047ca4e4
Jian He <jian.he@noaa.gov>
2025-05-08 18:58:43 -0600

    Updates on met IC

--- jobs/rocoto/getic_gsl_fv3gfsv15.sh
+++ jobs/rocoto/getic_gsl_fv3gfsv15.sh
@@ -22,7 +22,11 @@ echo
 yyyymmdd=`echo $CDATE | cut -c1-8`
 hh=`echo $CDATE | cut -c9-10`
 yyddd=`date +%y%j -u -d $yyyymmdd`
-fv3ic_dir=${ROTDIR}/${CDUMP}.${yyyymmdd}/${hh}/${COMPONENT}
+yyyy=`echo $CDATE | cut -c1-4`
+mm=`echo $CDATE | cut -c5-6`
+#fv3ic_dir=${ROTDIR}/${CDUMP}.${yyyymmdd}/${hh}/${COMPONENT}
+fv3ic_dir=${ROTDIR}/${CDUMP}.${yyyymmdd}/${hh}/
+hpssdir="/NCEPPROD/hpssprod/runhistory/rh$yyyy/$yyyy$mm/$PDY"
 
 ## create links in FV3ICS directory
 mkdir -p $fv3ic_dir
@@ -44,8 +48,58 @@ if [[ -f $PUBDIR/${pubsfc_file} ]]; then
 elif  [[ -f $RETRODIR/${pubsfc_file} ]]; then
   echo "linking $RETRODIR...."
   ln -fs $RETRODIR/${pubsfc_file} $sfc_file
-  ln -fs $RETRODIR/${pubatm_file} $atm_file 
+  ln -fs $RETRODIR/${pubatm_file} $atm_file
 else
-  echo "missing input files!"
-  exit 1
+        nfanal=2
+	if [[ $CDATE -le "2019061200"  ]]; then
+	  fanal[1]="./${CDUMP}.t${cyc}z.atmanl.nemsio"
+          fanal[2]="./${CDUMP}.t${cyc}z.sfcanl.nemsio"
+        else
+	  cd ${ROTDIR}
+          fanal[1]="./${CDUMP}.$yyyymmdd/$cyc/${CDUMP}.t${cyc}z.atmanl.nemsio"
+          fanal[2]="./${CDUMP}.$yyyymmdd/$cyc/${CDUMP}.t${cyc}z.sfcanl.nemsio"
+        fi
+
+        flanal="${fanal[1]} ${fanal[2]}"
+        tarpref="gpfs_dell1_nco_ops_com"
+        if [ $CDUMP = "gdas" ]; then
+            tarball="$hpssdir/${tarpref}_gfs_prod_${CDUMP}.${yyyymmdd}_${cyc}.${CDUMP}_nemsio.tar"
+        elif [ $CDUMP = "gfs" ]; then
+            tarball="$hpssdir/${tarpref}_gfs_prod_${CDUMP}.${yyyymmdd}_${cyc}.${CDUMP}_nemsioa.tar"
+        fi
+
+    # Get initial conditions from HPSS
+#    if [ $rc -ne 0 ]; then
+
+        # check if the tarball exists
+        hsi ls -l $tarball
+        rc=$?
+        if [ $rc -ne 0 ]; then
+            echo "$tarball does not exist and should, ABORT!"
+            exit $rc
+        fi
+        # get the tarball
+        htar -xvf $tarball $flanal
+        rc=$?
+        if [ $rc -ne 0 ]; then
+            echo "untarring $tarball failed, ABORT!"
+            exit $rc
+        fi
+
+        # Move the files to legacy EMC filenames
+       # for i in `seq 1 $nfanal`; do
+       #     $NMV ${fanal[i]} ${ftanal[i]}
+       # done
+
+#    fi
+
+    # If found, exit out
+    if [ $rc -ne 0 ]; then
+        echo "Unable to obtain operational GFS initial conditions, ABORT!"
+        exit 1
+    fi
+
+#else
+#  echo "missing input files!"
+#  exit 1
 fi
